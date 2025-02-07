f77b0d959a369d105f2a92031451d86a76c0be49
Yuk <dbrrmsdn51@naver.com>
2025-02-07 14:28:57 +0900

    feat : VO객체 작성

---
+++ src/main/java/com/miniproject/model/Member.java
@@ -0,0 +1,23 @@
+package com.miniproject.model;
+
+import java.sql.Timestamp;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Getter
+@Setter
+@ToString
+public class Member {
+	private String userId, userPwd, userName, mobile, email,  userImg;
+	private Timestamp registerDate;
+	private int userpoint;
+}
