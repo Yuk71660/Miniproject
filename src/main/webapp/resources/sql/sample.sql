15ba545ccd579f17929b2035cfff6c5b13c05579
Yuk <dbrrmsdn51@naver.com>
2025-02-07 09:45:07 +0900

    sql : 포인트 지급 기능 구현을위한 정보/지급내역 테이블 생성 및 관계구현 후 더미데이터 생성

--- src/main/webapp/resources/sql/sample.sql
+++ src/main/webapp/resources/sql/sample.sql
@@ -10,7 +10,7 @@ CREATE TABLE `member` (
   `registerDate` datetime DEFAULT CURRENT_TIMESTAMP,
   `userImg` varchar(50) DEFAULT 'memberimg/avatar100.png',
   PRIMARY KEY (`userId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='회원 테이블'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='회원 테이블';
 
 -- 게시글 테이블 생성
 CREATE TABLE `webmoonya`.`hboard` (
@@ -36,9 +36,50 @@ ADD CONSTRAINT `hboard_member_fk`
   ON DELETE SET NULL
   ON UPDATE NO ACTION;
   
-INSERT INTO `webmoonya`.`hboard` (`title`, `content`, `writer`) VALUES ('게시판', 'sdf', 'admin');
+INSERT INTO `webmoonya`.`hboard` (`title`, `content`, `writer`) VALUES ('날씨', '춥다', 'admin');
 
 SELECT * FROM hboard order by boardNo desc;
+
+-- 포인트 정보 테이블 생성
+CREATE TABLE `webmoonya`.`pointinfo` (
+  `pointcontent` VARCHAR(20) NOT NULL,
+  `pointscore` INT NOT NULL,
+  PRIMARY KEY (`pointcontent`));
+
+INSERT INTO `webmoonya`.`pointinfo` (`pointcontent`, `pointscore`) VALUES ('회원가입', '100');
+INSERT INTO `webmoonya`.`pointinfo` (`pointcontent`, `pointscore`) VALUES ('게시글작성', '5');
+INSERT INTO `webmoonya`.`pointinfo` (`pointcontent`, `pointscore`) VALUES ('로그인', '1');
+INSERT INTO `webmoonya`.`pointinfo` (`pointcontent`, `pointscore`) VALUES ('답글/댓글작성', '2');
+INSERT INTO `webmoonya`.`pointinfo` (`pointcontent`, `pointscore`) VALUES ('게시글신고', '-10');
+
+-- 포인트 지급 내역 테이블 생성
+CREATE TABLE `webmoonya`.`pointlog` (
+  `pointLogNo` INT NOT NULL AUTO_INCREMENT,
+  `pointwhen` DATETIME NOT NULL DEFAULT now(),
+  `pointwho` VARCHAR(8) NOT NULL,
+  `pointwhy` VARCHAR(20) NOT NULL,
+  `pointscore` INT NOT NULL,
+  PRIMARY KEY (`pointLogNo`))
+COMMENT = '포인트 지급 내역';
+
+ALTER TABLE `webmoonya`.`pointlog` 
+ADD INDEX `pointlog_pointwho_fk_idx` (`pointwho` ASC) VISIBLE,
+ADD INDEX `pointlog_pointwhy_fk_idx` (`pointwhy` ASC) VISIBLE;
+;
+ALTER TABLE `webmoonya`.`pointlog` 
+ADD CONSTRAINT `pointlog_pointwho_fk`
+  FOREIGN KEY (`pointwho`)
+  REFERENCES `webmoonya`.`member` (`userId`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `pointlog_pointwhy_fk`
+  FOREIGN KEY (`pointwhy`)
+  REFERENCES `webmoonya`.`pointinfo` (`pointcontent`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
   
+INSERT INTO `webmoonya`.`pointlog` (`pointLogNo`, `pointwhen`, `pointwho`, `pointwhy`, `pointscore`) VALUES ('2', '2025-02-05 14:25:44', 'subAdmin', '회원가입', '100');
+
+
   
   
