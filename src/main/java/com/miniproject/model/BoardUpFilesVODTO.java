047cd3f1e4e89187aa2a1dea1fd359c4ca60ae1f
Yuk <dbrrmsdn51@naver.com>
2025-02-10 12:45:48 +0900

    feat : 업로드파일 테이블에 처리하기위한 클래스 작성

---
+++ src/main/java/com/miniproject/model/BoardUpFilesVODTO.java
@@ -0,0 +1,27 @@
+package com.miniproject.model;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@ToString
+public class BoardUpFilesVODTO {
+	private int boardUpfileNo;
+	private String originalFileName;
+	private String newFileName;
+	private String thumbFileName;
+	private String ext;
+	private int size;
+	private String base64Image;
+	private int boardNo;
+
+}
